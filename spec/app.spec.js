750ff15d6406ad3c67917f78cef08a1756ce6191
Challen Bellamey <challenbellamey@hotmail.com>
2019-07-08 17:34:08 +0100

    add tests for get requests with no query on valid path

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,3 +1,27 @@
 const app = require('../app.js');
 const { expect } = require('chai');
 const request = require('supertest')(app);
+
+describe('/api', () => {
+
+    it('GET returns (200) information on how to use', () => {
+        return request
+            .get('api')
+            .expect(200)
+            .then(({ body }) => {
+                expect(body.message).to.equal('Hello! Please attach a query to your get request to receive a roman numeral conversion, i.e. /api/roman-numeral-to-integer?rn=CC');
+            })
+    });
+
+    describe('/roman-numeral-to-integer', () => {
+
+        it('GET returns (200) information on how to use', () => {
+            return request
+                .get('api/roman-numeral-to-integer')
+                .expect(200)
+                .then(({ body }) => {
+                    expect(body.message).to.equal('Hello! Please attach a query to your get request to receive a roman numeral conversion, i.e. /api/roman-numeral-to-integer?rn=CC');
+                })
+        });
+    })
+})
