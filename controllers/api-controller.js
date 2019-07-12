b9d1de57ad044e75490d52cfae140022788738de
Challen Bellamey <challenbellamey@hotmail.com>
2019-07-12 07:58:36 +0100

    Correct path in getInfo response message.

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,6 +1,6 @@
 
 const getInfo = (req, res, next) => {
-    const message = `Hello! Please attach a query to your get request to receive a Roman numerals conversion, i.e. ${req.get('host')}/get/roman-numerals-to-integer?rn=CC`;
+    const message = `Hello! Please attach a query to your get request to receive a Roman numerals conversion, i.e. ${req.get('host')}/api/roman-numerals-to-integer?rn=CC`;
 
     res.status(200).send({ message });
 };
